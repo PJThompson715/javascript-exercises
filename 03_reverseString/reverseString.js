25ff5d67cc14f390028c312d56b3f1afaf529818
PJThompson715 <pthompson715@gmail.com>
2023-08-22 14:10:51 -0400

    03 finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) 
+{
+    let reverse="";
+    let length=string.length;
+    for (let i=length-1;i>=0;i--)
+    {
+        reverse+=string[i];
+    }
+    return reverse;
 
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
