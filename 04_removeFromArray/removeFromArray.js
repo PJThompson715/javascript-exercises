1df75083fcb5310e4e11a5007d8481340761c34b
PJThompson715 <pthompson715@gmail.com>
2023-08-22 16:20:25 -0400

    finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...index) 
+{
+    let newarray=[];
 
-};
+    arr.forEach((item)=> {
+        if(!index.includes(item))
+        {
+            newarray.push(item)
+        }
+    });
+    return newarray
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
