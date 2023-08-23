7c46f9379fa5315d64fedea3f96d63c0408684c4
PJThompson715 <pthompson715@gmail.com>
2023-08-23 08:27:23 -0400

    finished 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) 
+{
+  let c=(temp-32)*5/9;
+  c=Math.round(c * 10)/10
+  return c;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) 
+{
+  let f=(temp*1.8)+32;
+  f=Math.round(f*10)/10;
+  return f;
 };
 
 // Do not edit below this line
