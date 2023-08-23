c829a45aafb1092325ad8055865533eeb2fc844a
Parker <pthompson715@gmail.com>
2023-08-22 20:23:47 -0400

    05 finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(first, last) 
+{
+    
+    if(Number.isInteger( last) == false || Number.isInteger( first) == false)
+    {
+        return "ERROR";
+    }
 
+    
+    if (first<0)
+    {
+        return "ERROR";
+    }
+    if(first>last)
+    {
+        let temp=first;
+        first=last;
+        last=temp;
+    }
+    let sum=0;
+    for (let i=first;first<=last;first++)
+    {
+        sum+=first;
+    }
+    return sum;
 };
 
 // Do not edit below this line
